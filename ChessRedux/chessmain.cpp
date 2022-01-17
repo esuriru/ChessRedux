cd6bbac2b11cc6d809d0775e9b832965c3152286
esuriru <logicismmc@gmail.com>
2022-01-17 14:52:03 +0800

    argus is mad

--- ChessRedux/chessmain.cpp
+++ ChessRedux/chessmain.cpp
@@ -6,16 +6,85 @@
 enum class PieceType { PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING, EMPTY };
 enum class Colour { BLACK, WHITE };
 
+class ChessGame {
+private:
+    ChessBoard chessboard;
+    bool validNewPieceLocation(std::pair<int, int> pieceLocation, std::pair<int, int> newPieceLocation) {
+        for (int i = 0; i < chessboard.getPiece(pieceLocation).getValidMoves().size(); i++)
+        {
+
+            if (chessboard.getPiece(pieceLocation).getColour() != chessboard.getPiece(newPieceLocation).getColour() && newPieceLocation == chessboard.getPiece(pieceLocation).getValidMoves()[i]) {
+                //if the movement is from one colour to another, and that chess piece location is in that type's valid moves, then return true
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+ 
+    bool isMoveCheck(std::pair<int, int> pieceLocation, std::pair<int, int> newPieceLocation) {};
+    bool isMoveCheckMate(std::pair<int, int> pieceLocation, std::pair<int, int> newPieceLocation) {};
+
+public:
+    void pieceMove(std::pair<int, int> pieceLocation, std::pair<int, int> newPieceLocation) {
+        if (validNewPieceLocation(pieceLocation, newPieceLocation) && chessboard.isTherePieceHere(pieceLocation)) {
+            PieceType movingPieceType = chessboard.getPiece(pieceLocation).getPieceType();
+            chessboard.setPiece(pieceLocation, newPieceLocation);
+            /*switch (movingPieceType) {
+            case PieceType::PAWN:
+                //is this valid? return true if yes, false if no
+            case PieceType::BISHOP:
+                //
+            case PieceType::KNIGHT:
+
+
+            }*/
+        }
+    };
+   
+    static void newGame();
+    void makeMove(pair<int, int> pieceLocation, pair<int, int> newPieceLocation);
+};
+
 class ChessPiece {
 private:
     PieceType piecetype;
     Colour colour;
     std::pair<int, int> pieceLocation;
+    std::vector<std::pair<int, int>> validMoves;
 
 
 public:
-    ChessPiece(){
-        piecetype = PieceType::EMPTY;
+    
+    ChessPiece(PieceType type){
+        piecetype = type;
+        switch (type) {
+        case PieceType::KNIGHT:
+            //add vertical Ls
+            //invalid moves are also included
+            std::pair<int, int> upleftL;
+            upleftL.first = pieceLocation.first - 1;
+            upleftL.second = pieceLocation.second + 2;
+            validMoves.push_back(upleftL);
+            std::pair<int, int> uprightL;
+            uprightL.first = pieceLocation.first + 1;
+            uprightL.second = pieceLocation.second + 2;
+            validMoves.push_back(uprightL);
+            std::pair<int, int> downleftL;
+            downleftL.first = pieceLocation.first - 1;
+            downleftL.second = pieceLocation.first - 2;
+            validMoves.push_back(downleftL);
+            std::pair<int, int> downrightL;
+            downrightL.first = pieceLocation.first - 1;
+            downrightL.second = pieceLocation.first - 2;
+            validMoves.push_back(downrightL);
+            
+            
+        }
+    }
+    std::vector<std::pair<int, int>> getValidMoves() {
+        return validMoves;
     }
     PieceType getPieceType() {
         return piecetype;
@@ -46,6 +115,7 @@ private:
 public:
     ChessBoard() {
         board[8][8];
+        ChessPiece whiteRook1 = ChessPiece(PieceType::ROOK);
     }
     void showBoard();
     void resetBoard();
@@ -57,7 +127,7 @@ public:
         }
     }
     ChessPiece getPiece(std::pair<int, int> chessPieceLocation) {
-        return board[chessPieceLocation.first][chessPieceLocation.second].getPieceType();
+        return board[chessPieceLocation.first][chessPieceLocation.second];
     }
     void setPiece(std::pair<int, int> chessPieceLocation, std::pair<int, int> newLocation) {
         if (isTherePieceHere(chessPieceLocation) != 1) {
@@ -68,37 +138,16 @@ public:
                 std::cout << "not possible"; //.. is trying to take their own team
             }
             else {
-                board[newLocation.first][newLocation.second] = ChessPiece();
+                board[newLocation.first][newLocation.second] = ChessPiece(PieceType::EMPTY);
                 board[chessPieceLocation.first][chessPieceLocation.second].setPieceLocation(newLocation);
             }
         }
     }
 };
 
-class ChessGame {
-private:
-    ChessBoard chessboard;
-    bool isMoveLegal(std::pair<int, int> pieceLocation, std::pair<int, int> newPieceLocation) {
-        if (validPieceLocation() && validNewPieceLocation() && pieceLocationchessboard.isTherePieceHere(pieceLocation)) {
-            ChessPiece movingPieceType = chessboard.getPiece(pieceLocation).getPieceType();
-            switch (movingPieceType) {
-            case PieceType::PAWN:
-                //is this valid? return true if yes, false if no
-            case PieceType::BISHOP:
-                ...
-                    ...
-            }
-        }
-    };
-    bool isMoveCheck(std::pair<int, int> pieceLocation, std::pair<int, int> newPieceLocation) {};
-    bool isMoveCheckMate(std::pair<int, int> pieceLocation, std::pair<int, int> newPieceLocation) {};
-
-public:
-    void newGame();
-    void makeMove(pair<int, int> pieceLocation, pair<int, int> newPieceLocation);
-};
 
 int main()
 {
+    ChessGame::newGame();
 
 }
